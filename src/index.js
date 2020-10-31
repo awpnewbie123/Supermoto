a049b3ad176077581ddc873624aac1cdaef542f9
awpnewbie123 <leticia05@mail.ru>
2020-11-01 01:20:21 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import $ from "jquery";
+import popper from "popper.js";
+import bootstrap from "bootstrap";
+// import "babel";
+import "catalog";
+import './scss/main.scss';
+
+// $(document).ready(function(){
+//   $('a[href^="#"], *[data-href^="#"]').on('click', function(e){
+//       e.preventDefault();
+//       var t = 10000;
+//       var d = $(this).attr('data-href') ? $(this).attr('data-href') : $(this).attr('href');
+//       $('html,body').stop().animate({ scrollTop: $(d).offset().top }, t);
+//   });
+// });
